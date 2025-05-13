31d72612fa359d86ef7739c5a9f9eb180f3c4a47
student <student@FJ-1027>
2025-05-13 14:41:43 +0900

    add feature/#1

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Ver. 3");
+		System.out.println("feature/#1");
 		
 	}
 
