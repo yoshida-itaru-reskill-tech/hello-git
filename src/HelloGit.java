268152338c4fc29572059a72399d76e8dccba354
student <student@FJ-1027>
2025-05-14 13:05:49 +0900

    update for #4

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,11 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Ver. 3");
-		System.out.println("feature/#1");
-		System.out.println("feature/#2");
-		System.out.println("feature/#3");
 		System.out.println("feature/#4");
-		System.out.println("feature/#2");
 	}
 
 }
